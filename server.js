865b757f0c0f0ccf4457b6603d30d91d762ae18a
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-08-26 05:00:52 -0500

    --

--- server.js
+++ server.js
@@ -35,11 +35,19 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res)=> {
-	res.send('this is working');
-})
+// app.get('/', (req,res)=> {
+// 	res.send('this is working');
+// })
 
+
+// app.listen(process.env.PORT || 3000, ()=> {
+// 	console.log('app is running on port ${process.env.PORT}');
+// })
+
+
+
+app.use(express.static(__dirname + '/germanium'));
 
 app.listen(process.env.PORT || 3000, ()=> {
-	console.log('app is running on port ${process.env.PORT}');
+	console.log(`app is running on port ${process.env.PORT}`);
 })
