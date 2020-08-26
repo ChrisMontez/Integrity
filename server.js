7ad189d5bb2fc6d54d641a05e1a1fbe9ca1f1bb9
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-08-26 04:49:19 -0500

    --

--- server.js
+++ server.js
@@ -1,33 +1,45 @@
+// const express = require('express');
+// // const cors = require('cors');
+// const app = express();
+// // const bodyParser = require('body-parser');
+// // app.use(cors())
+// // app.use(bodyParser.json());
+
+
+// app.use(express.static(__dirname + '/germanium'));
+
+// app.listen(process.env.PORT || 3000, ()=> {
+// 	console.log(`app is running on port ${process.env.PORT}`);
+// })
+
+
+// app.use(express.urlencoded({
+// 	extended: false
+// }));
+// app.use(express.json());
+
+
+
+
+// // app.get('/', (req, res) => {
+// // 	res.sendFile(path.join(__dirname, 'views', 'index1.html'));
+// // });
+
+
+
+
+
+
+
 const express = require('express');
-// const cors = require('cors');
+
 const app = express();
-// const bodyParser = require('body-parser');
-// app.use(cors())
-// app.use(bodyParser.json());
 
-
-app.use(express.static(__dirname + '/germanium'));
-
-app.listen(process.env.PORT || 3000, ()=> {
-	console.log(`app is running on port ${process.env.PORT}`);
+app.get('/', (req,res)=> {
+	res.send('this is working');
 })
 
 
-app.use(express.urlencoded({
-	extended: false
-}));
-app.use(express.json());
-
-
-
-
-// app.get('/', (req, res) => {
-// 	res.sendFile(path.join(__dirname, 'views', 'index1.html'));
-// });
-
-
-
-
-
-
-
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log('app is running on port ${process.env.PORT}');
+})
