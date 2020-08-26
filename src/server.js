09229182e6a3109d1a33c4925982c14536d31719
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-08-26 15:02:44 -0500

    --

---
+++ src/server.js
@@ -0,0 +1,35 @@
+const path = require('path')
+const express = require('express');
+// const cors = require('cors');
+const app = express();
+// const bodyParser = require('body-parser');
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../HTML'))
+
+// app.use(cors())
+// app.use(bodyParser.json());
+
+
+app.use(express.static(path.join(__dirname, '../HTML')));
+
+
+
+// app.use(express.urlencoded({
+// 	extended: false
+// }));
+
+
+
+// app.use(express.json());
+
+
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'views', '..index1.html'));
+// });
+
+
+
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
