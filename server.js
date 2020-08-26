627dff09bfa0129b33fc223cc4b6a83110eed555
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-08-26 04:31:04 -0500

    npm init

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(cors())
+app.use(bodyParser.json());
+
+
+app.use(express.static(__dirname + 'index1'));
+
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`app is running on port ${process.env.PORT}`);
+})
+
+
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'index1.html'));
+});
+
+
+
+
+
+
+
