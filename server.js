200773198687b9e3dba26668c0cf1ecd5145a86c
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-08-26 04:36:53 -0500

    --

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(cors())
 app.use(bodyParser.json());
 
 
-app.use(express.static(__dirname + 'index1'));
+app.use(express.static(__dirname + '/germanium'));
 
 app.listen(process.env.PORT || 3000, ()=> {
 	console.log(`app is running on port ${process.env.PORT}`);
@@ -21,9 +21,9 @@ app.use(express.json());
 
 
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'views', 'index1.html'));
-});
+// app.get('/', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'views', 'index1.html'));
+// });
 
 
 
