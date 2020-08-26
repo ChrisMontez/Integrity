c1dea81823da7add1ae56c31aabb825b46816859
ChrisMontez <64094989+ChrisMontez@users.noreply.github.com>
2020-08-26 11:58:18 -0500

    --

--- server.js
+++ server.js
@@ -1,53 +1,43 @@
 // const express = require('express');
-// // const cors = require('cors');
 // const app = express();
-// // const bodyParser = require('body-parser');
-// // app.use(cors())
-// // app.use(bodyParser.json());
 
 
-// app.use(express.static(__dirname + '/germanium'));
+
+// app.use(express.static(__dirname + '/HTML'));
 
 // app.listen(process.env.PORT || 3000, ()=> {
 // 	console.log(`app is running on port ${process.env.PORT}`);
 // })
 
 
-// app.use(express.urlencoded({
-// 	extended: false
-// }));
-// app.use(express.json());
-
-
-
-
-// // app.get('/', (req, res) => {
-// // 	res.sendFile(path.join(__dirname, 'views', 'index1.html'));
-// // });
-
-
-
-
-
 
 
 const express = require('express');
-
+// const cors = require('cors');
 const app = express();
-
-// app.get('/', (req,res)=> {
-// 	res.send('this is working');
-// })
-
-
-// app.listen(process.env.PORT || 3000, ()=> {
-// 	console.log('app is running on port ${process.env.PORT}');
-// })
+// const bodyParser = require('body-parser');
+// app.use(cors())
+// app.use(bodyParser.json());
 
 
-
-app.use(express.static(__dirname + '/HTML'));
+app.use(express.static(__dirname + '/germanium/HTML'));
 
 app.listen(process.env.PORT || 3000, ()=> {
 	console.log(`app is running on port ${process.env.PORT}`);
 })
+
+
+app.use(express.urlencoded({
+	extended: false
+}));
+app.use(express.json());
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views', 'index1.html'));
+});
